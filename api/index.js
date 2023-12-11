b4655e229e03e78845a18b7dc9f66a543be47505
akyua <gabrielnovatoo@gmail.com>
2023-12-11 19:40:03 -0300

    creating backend and database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
+const mongodbServer = process.env.MONGODB_SERVER;
 
 app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('test ok2');
-    res
+mongoose.connect(mongodbServer);
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password
+        });
+        res.json(userDoc);
+    } catch(error){
+        res.status(400).json(error);
+    }
 })
 
 app.listen(4000);
--- client/src/components/Register/RegisterComponent.js
+++ client/src/components/Register/RegisterComponent.js
@@ -1,9 +1,22 @@
+import {useState} from 'react';
+
 export default function RegisterComponent(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+   async function register(ev){
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        })
+        console.log(response);
+    }
     return(
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
+            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
             <button>Register</button>
         </form>
     )
