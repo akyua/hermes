4a1ffb9d94905946b62b887d2c5662c927ef57fc
akyua <gabrielnovatoo@gmail.com>
2023-12-13 10:41:52 -0300

    checked if logged

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const User = require('./models/User');
 const app = express();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const mongodbServer = process.env.MONGODB_SERVER;
 
 const salt = bcrypt.genSaltSync(10);
@@ -14,6 +15,7 @@ const secret = 'randaodsmadmaskdsa'
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(mongodbServer);
 app.post('/register', async (req, res) => {
@@ -41,6 +43,14 @@ app.post('/login', async (req, res) => {
     } else{
         res.status(400).json('Wrong Credentials')
     }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if(err) throw err;
+        res.json(info);
+    });
 })
 
 app.listen(4000);
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 function Header(){
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/profile',{
+            credentials: 'include'
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            })
+        })
+    }, []);
     return(
         <header>
             <a href="/" className="logo">Hermes</a>
             <nav>
-                <a href="/login">Login</a>
-                <a href="/register">Register</a>
+                {username && (
+                    <>
+                    <Link to='/create'>Create new post</Link>
+                    <a>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <a href="/login">Login</a>
+                        <a href="/register">Register</a>
+                    </>
+                )}
+                
             </nav>
         </header>
     )
