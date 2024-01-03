dad8147b283f1808044c36cdd8b587c432a909a7
akyua <gabrielnovatoo@gmail.com>
2024-01-03 20:10:31 -0300

    add link to id of the post

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,14 +1,19 @@
 import './Post.scss';
-import {format} from "date-fns";
+import {format} from "date-fns"; 
+import {Link} from "react-router-dom";
 
-function Post({title, summary, cover, content, createdAt, author}){
+function Post({_id, title, summary, cover, content, createdAt, author}){
     return(
         <div className='post'>
             <div className="image">
-                <img src={'http://localhost:4000/'+cover} alt="image" />
+                <Link to={`/post/${_id}`}>
+                    <img src={'http://localhost:4000/'+cover} alt="image" />
+                </Link>
             </div>
             <div className="texts">  
-                <h2>{title}</h2>
+                <Link to={`/post/${_id}`}>
+                    <h2>{title}</h2>
+                </Link>
                 <p className='info'>
                     <span className='author'>{author.username}</span>
                     <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
